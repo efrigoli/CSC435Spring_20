d07833e8ab3848edfd0e6526603e3f43f06fb64c
Elise Frigoli <elise.frigoli@gmail.com>
2020-03-21 21:52:34 -0500

    Adding header content

--- src/header.js
+++ src/header.js
@@ -3,14 +3,17 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <nav>
-      <NavLink exact activeClassName="active" to="/">
-        Home
-      </NavLink>
-      <NavLink activeClassName="active" to="/login">
-        Login
-      </NavLink>
-    </nav>
+    <header>
+      <nav>
+        <h3>CSC-435</h3>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+        <NavLink activeClassName="active" to="/login">
+          Log In
+        </NavLink>
+      </nav>
+    </header>
   );
 }
 export default Header;
