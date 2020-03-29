b79b3713148e3a499089647692996229f874f9b7
Elise Frigoli <elise.frigoli@gmail.com>
2020-03-28 20:20:20 -0500

    Footer and Index updates

--- src/footer.js
+++ src/footer.js
@@ -1,18 +1,41 @@
-// Elise Frigoli
-// CSC 435 - Advanced Web App Development
-// Created: 03/21/20
-// Last Modified: 03/22/20 - Added Comments
+/* Elise Frigoli
+ * CSC 435 - Advanced Web App Development
+ * Created: 03/21/20
+ * Last Modified: 03/22/20 - Added Comments
+ *                03/28/20 - Changed Footer component from function to class
+ *                         - Added Contact information
+ */
 
 // Importing dependencies
 import React from "react";
 
 // Creating the Footer component to render the footer
-function Footer() {
-  return (
-    <footer>
-      <p>&copy;{new Date().getFullYear()} Elise Frigoli</p>
-    </footer>
-  );
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="contactInfo">
+          <h3>Call Us:</h3>
+          <p>{this.props.contactData.phone}</p>
+        </div>
+        <div className="contactInfo">
+          <h3>Email Us:</h3>
+          <ul>{this.props.contactData.emailAddresses.map((email, i) => <li key={i}>{email}</li>)}</ul>
+        </div>
+        <div className="contactInfo">
+          <h3>Visit Us:</h3>
+          <p>{this.props.contactData.street}
+            <br />{this.props.contactData.city}, {this.props.contactData.state}
+            <br />{this.props.contactData.zip}
+          </p>
+        </div>
+        <div className="copyrightInfo">
+          <p>All images are from <a href="https://unsplash.com/" target="_blank">Unsplash</a> and are freely licensed for both commercial and non-commercial use.</p>
+          <p>&copy;{new Date().getFullYear()} Elise Frigoli</p>
+        </div>
+      </footer>
+    )
+  }
 }
 
 export default Footer;
